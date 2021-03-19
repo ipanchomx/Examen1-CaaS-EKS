1b1265d2d83e9b253dbee9c71f643d252c665298
Edgar Medina <is715468@iteso.mx>
2021-03-19 01:40:27 -0600

    added function to get data from tone analyzer

--- src/public/index.js
+++ src/public/index.js
@@ -1,2 +1,50 @@
 let divEmotion = document.getElementById('divEmotion');
+let text = document.getElementById('idText');
+let data;
+let tone;
+let score;
+let tone_name;
 
+
+let sendData = function() {
+    console.log(text.value);
+    if(text.value == "") return alert("You must write something in English!");
+
+    let data = {
+        text : text.value
+    }
+
+    axios({
+        method: 'post',
+        url: 'http://medina-rifas-tone-analyzer.mybluemix.net/tone-analyzer',
+        data : {
+            text: text.value
+        }
+    })
+    .then((res) => {
+        // console.log(res);
+        // console.log(res.data.data);
+        data = JSON.parse(res.data.data);
+        // console.log('result:');
+        // console.log(data);
+        // console.log(data.result);
+        tone = "";
+        if(data.result.document_tone.tone_categories != null) {
+            tone = data.result.document_tone.tone_categories[0].tones[0];
+
+            // score = tone.score;
+            // tone_name = tone.tone_name;
+
+            data.result.document_tone.tone_categories[0].tones.forEach(element => {
+                // console.log(element.score)
+                // console.log(element.tone_name)
+                score = element.score * 100 + "";
+
+                divEmotion.innerHTML += `<h5> ${element.tone_name}: ${score.substring(0,4)}%</h5>`
+            });
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+    })  
+}
